00e1b306b4eb0b0e9348b6094d7f9f661689539e
William Hemmingsson <william.hemmingsson@gmail.com>
2025-12-14 15:50:45 +0100

    More setup code for day 12

--- src/puzzles/day12.ts
+++ src/puzzles/day12.ts
@@ -8,17 +8,49 @@ labels.set(3, "D");
 labels.set(4, "E");
 labels.set(5, "F");
 
+interface Position {
+  x: number;
+  y: number;
+}
+
+enum Degrees {
+  D90 = 1,
+  D180 = 2,
+  D270 = 3,
+}
+
+const rawPositions: Position[] = [];
+rawPositions.push({ x: 0, y: 0 });
+rawPositions.push({ x: 1, y: 0 });
+rawPositions.push({ x: 2, y: 0 });
+rawPositions.push({ x: 0, y: 1 });
+rawPositions.push({ x: 1, y: 1 });
+rawPositions.push({ x: 2, y: 1 });
+rawPositions.push({ x: 0, y: 2 });
+rawPositions.push({ x: 1, y: 2 });
+rawPositions.push({ x: 2, y: 2 });
+
+const getRawPositions = (s: Shape) => {
+  return rawPositions.filter((pos) => s.matrix![pos.y]![pos.x] === 1);
+};
+
 class Grid {
   width: number;
   height: number;
   order: number[];
   shapes: Shape[];
+  matrix: number[][];
+  history: number[][][];
 
   constructor(w: number, h: number, order: number[]) {
     this.width = w;
     this.height = h;
     this.order = order;
     this.shapes = [];
+    this.matrix = [];
+    this.history = [];
+
+    this._constructMatrix();
   }
 
   loadShapes() {
@@ -35,6 +67,24 @@ class Grid {
     }
   }
 
+  setAt(x: number, y: number) {
+    this.matrix[y]![x] = 1;
+  }
+
+  _constructMatrix() {
+    let row = "0"
+      .repeat(this.width)
+      .split("")
+      .map((v) => v.toInt());
+    for (let r = 0; r < this.height; r++) {
+      this.matrix.push(row);
+    }
+  }
+
+  resetMatrix() {
+    this.matrix = [];
+  }
+
   print() {
     console.log(`Grid ${this.width}x${this.height}`);
     const groups = this.shapes.groupBy((e) => e.getLabel());
@@ -43,6 +93,49 @@ class Grid {
       groups.map((g) => `${g.label as string}:${g.elements.length}`)
     );
   }
+
+  printMatrix() {
+    console.log(`Grid ${this.width}x${this.height}`);
+    for (let r = 0; r < this.height; r++) {
+      console.log(this.matrix[r]!.join(" "));
+    }
+  }
+
+  canPlaceShapeAt(s: Shape, x: number, y: number) {
+    // Construct positions on grid
+    const gridPositions = getRawPositions(s).map((gp) => {
+      return { x: gp.x + x, y: gp.y + y } as Position;
+    });
+
+    return gridPositions.every(
+      (gp) =>
+        gp.y >= 0 &&
+        gp.y < this.height &&
+        gp.x >= 0 &&
+        gp.x < this.width &&
+        this.matrix![gp.y]![gp.x] === 0
+    );
+  }
+
+  undo() {
+    const prevState = this.history.pop();
+    if (prevState) {
+      this.matrix = prevState;
+    } else {
+      console.log("Unable to undo! [prev state is undefined]");
+    }
+  }
+
+  placeShape(s: Shape, x: number, y: number) {
+    // Place the current "state" on the history stack
+    this.history.push(this.matrix);
+    // Assume s can be placed
+    getRawPositions(s)
+      .map((gp) => {
+        return { x: gp.x + x, y: gp.y + y } as Position;
+      })
+      .forEach((gp) => this.setAt(gp.x, gp.y));
+  }
 }
 
 class Shape {
@@ -50,11 +143,13 @@ class Shape {
   id: number;
   label: string;
   rows: string[];
+  rowsOriginal: string[];
   constructor(id: number, label: string, rows: string[]) {
     this.id = id;
     this.label = label;
     this.matrix = [];
     this.rows = rows;
+    this.rowsOriginal = [...rows];
     this._constructMatrix(this.rows);
   }
 
@@ -70,9 +165,9 @@ class Shape {
     console.log(`Piece ${this.id} (${this.label}):`);
     this.matrix.forEach((r) => {
       if (useLabel) {
-        console.log(r.map((c) => (c === 1 ? this.label : " ")).join(""));
+        console.log(r.map((c) => (c === 1 ? this.label : " ")).join(" "));
       } else {
-        console.log(r.join(""));
+        console.log(r.join(" "));
       }
     });
     console.log();
@@ -86,8 +181,26 @@ class Shape {
     return new Shape(this.id, this.label, this.rows);
   }
 
+  _getRotatedRows(rows: string[]) {
+    const newRows = [];
+    newRows.push(rows[2]![0]! + rows[1]![0]! + rows[0]![0]!);
+    newRows.push(rows[2]![1]! + rows[1]![1]! + rows[0]![1]!);
+    newRows.push(rows[2]![2]! + rows[1]![2]! + rows[0]![2]!);
+    return newRows;
+  }
+
   rotate() {
-    // Rotate the shape 90 deg CW
+    const newRows = this._getRotatedRows(this.rows);
+    this._constructMatrix(newRows);
+  }
+
+  rotateDeg(degrees: Degrees) {
+    let rows = this.rows;
+    for (let i = 0; i < degrees; i++) {
+      rows = this._getRotatedRows(rows);
+    }
+    this.rows = rows;
+    this._constructMatrix(rows);
   }
 
   flipOnY() {
@@ -96,22 +209,24 @@ class Shape {
     const tmp = rowsCopy[0]!;
     rowsCopy[0] = rowsCopy[2]!;
     rowsCopy[2] = tmp;
+    this.rows = rowsCopy;
     this._constructMatrix(rowsCopy);
   }
 
   flipOnX() {
     // Flip on the x-axis
-    const rowsCopy: string[] = [];
+    const newRows: string[] = [];
     for (let r = 0; r < this.rows.length; r++) {
       let rr = this.rows[r]!;
       let newRow = rr[2]! + rr[1]! + rr[0];
-      rowsCopy.push(newRow);
+      newRows.push(newRow);
     }
-    this._constructMatrix(rowsCopy);
+    this.rows = newRows;
+    this._constructMatrix(newRows);
   }
 
   reset() {
-    this._constructMatrix(this.rows);
+    this._constructMatrix(this.rowsOriginal);
   }
 }
 
@@ -170,11 +285,6 @@ const solveFunc = async (d: any) => {
   // Debug logging
   printShapes();
   printGrids();
-
-  // Testing
-  shapes[0]?.flipOnY();
-  shapes[0]?.flipOnX();
-  shapes[0]?.print(true);
 };
 
 const puzzle = new Puzzle(12, solveFunc, SourceType.STRING_ARRAY, true);
